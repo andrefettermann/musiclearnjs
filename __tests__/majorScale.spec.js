286900fb73a37a6a8a1d9ec413f5daaa70f06394
andre.fettermann <andre.fettermann@trt1.jus.br>
2021-07-28 11:22:10 -0300

    UI correction

--- __tests__/majorScale.spec.js
+++ __tests__/majorScale.spec.js
@@ -1,24 +1,10 @@
-import { majorScaleBuilder } from "../buildMajorScale";
-import { majorChordBuilder } from "../buildMajorChord";
-
-import {MajorBuilder} from "../MajorBuilder";
+import {MajorBuilder} from "../MajorBuilder.js";
 
 //Executar todos os testes com 'npm test'
 
 describe("Build a major scale", () => {
     
     test("should build a major note scale", () => {
-        /*
-        const input = "F#";
-
-        //expected
-        //const output = "C#,D#,F,F#,G#,A#,C,C#";   // let input = "C#"
-        //const output = "C,D,E,F,G,A,B,C";         // let input = "C"
-        //const output = "B,C#,D#,E,F#,G#,A#,B";    // let input = "B"
-        const output = "F#,G#,A#,B,C#,D#,F,F#";     // let input = "F#"
-
-        expect(majorScaleBuilder(input)).toBe(output);
-        */
         const input = "F#";
         const output = "F#,G#,A#,B,C#,D#,F,F#";     // let input = "F#"
 
@@ -28,9 +14,6 @@ describe("Build a major scale", () => {
     });
 
     test("should build a major chord", () => {
-        
-        //const scale = majorScaleBuilder(input);
-
         const input = "D";
         const output = "D,F#,A";
         const majorBuilder = new MajorBuilder();
--- musiclearn.js
+++ musiclearn.js
@@ -1,3 +1,4 @@
+/*
 var noteLetter = document.getElementById("note");
 var run = document.getElementById("run");
 var major_scale = document.getElementById("major_scale");
@@ -9,6 +10,26 @@ run.addEventListener("click", function() {
 
     let majorScale = buildMajorScale(note);
     let majorChord = buildMajorChord(majorScale);
+
+    major_scale.innerText = "Major scale for " + note + " is " + majorScale;    
+    major_chord.innerText = "Major chord for " + note + " is " + majorChord;    
+    
+});
+*/
+import {MajorBuilder} from "./MajorBuilder.js";
+
+const noteLetter = document.getElementById("note");
+const run = document.getElementById("run");
+const major_scale = document.getElementById("major_scale");
+const major_chord = document.getElementById("major_chord");
+
+run.addEventListener("click", function() {
+    const majorBuilder = new MajorBuilder();
+
+    const note = noteLetter.value;
+
+    const majorScale = majorBuilder.buildScale(note);
+    const majorChord = majorBuilder.buildChord(majorScale);
 
     major_scale.innerText = "Major scale for " + note + " is " + majorScale;    
     major_chord.innerText = "Major chord for " + note + " is " + majorChord;    
